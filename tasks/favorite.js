d4269321b3721e10064080eb525f85a242ee5272
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-16 12:05:18 +0100

    Add favorite task

---
+++ tasks/favorite.js
@@ -0,0 +1,8 @@
+let favoriteColor = "Blue";
+let favoriteFood = "Ogbono Soup"
+let favoriteSport = "Football"
+let favoriteMovie = "The Beekeeper"
+
+console.log(
+    `My favorite color is ${favoriteColor}, I love eating ${favoriteFood}, I enjoy ${favoriteSport}, and my favorite movie is ${favoriteMovie}`
+)
