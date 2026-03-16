81f2226b4bbfcd62c95e6336c0f6817b89e958b7
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-16 12:58:49 +0100

    Add wordlength task

---
+++ tasks/wordLength.js
@@ -0,0 +1,10 @@
+let word = "hi";
+
+if (word.length < 5){
+    console.log(word,"is short");
+}else if( word.length >= 5 && word.length <=8){
+    console.log(word, "is medium")
+}
+else{
+    console.log(word, "is long")
+}
