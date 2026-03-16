65da07dce9b0e9172afa68a26eaae1844a8ee61a
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-16 12:33:41 +0100

    Add Circle Task

---
+++ tasks/circle.js
@@ -0,0 +1,8 @@
+let radius = 5;
+
+let area = Math.PI * radius ** 2;
+let perimeter = 2 * Math.PI * radius;
+
+
+console.log("Area =", area.toFixed(2));
+console.log("Perimeter =", perimeter.toFixed(2));
