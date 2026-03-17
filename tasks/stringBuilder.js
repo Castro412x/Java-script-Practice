d8d609223b8b28a4ced55e99c83158fb5ac3d146
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-17 13:17:23 +0100

    Add String Builder Task

---
+++ tasks/stringBuilder.js
@@ -0,0 +1,17 @@
+let char = "*";
+let count = 6;
+let result = "";
+
+for (let i = 0; i < count; i++){
+    result += char ;
+}
+console.log(result);
+
+char = "-";
+count = 10;
+result = "";
+
+for (let i = 0; i < count; i++){
+    result += char ;
+}
+console.log(result);
