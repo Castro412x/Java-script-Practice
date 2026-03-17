5016db48367b0150e1428bb24ee767824ed8b39b
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-17 12:54:06 +0100

    Add Numbers to Word task

---
+++ tasks/numWord.js
@@ -0,0 +1,22 @@
+let word = "111111111"
+if (word.length === 0){
+    console.log("Zero")
+}else if(word.length === 1){
+    console.log("One")
+}else if (word.length === 2){
+    console.log("Two")
+}else if(word.length === 3){
+    console.log("Three")
+}else if(word.length === 4){
+    console.log("Four")
+}else if(word.length === 5){
+    console.log("Five")
+}else if(word.length === 6){
+    console.log("Six")
+}else if(word.length === 7){
+    console.log("Seven")
+}else if(word.length === 8){
+    console.log("Eight")
+}else if(word.length === 9){
+    console.log("Nine")
+}else(console.log ("Unknown"))
